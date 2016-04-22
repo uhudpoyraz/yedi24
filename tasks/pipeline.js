b0c1a4037c0645377c55ba8c40654c9bd4894138
tanyildizc <tanyildizc@gmail.com>
2016-04-23 02:56:09 +0300

    Coklu Layout js order düzenlemesi

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -46,11 +46,22 @@ var jsAdminFilesToInject = [
   //'jsAdmin/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'admin/js/dependencies/**/*.js',
-
-  // All of the rest of your client-side js files
-  // will be injected here in no particular order.
-  'admin/js/**/*.js'
+  
+  'admin/js/jquery.min.js',
+  'admin/js/jquery-ui-1.10.3.min.js',
+  'admin/js/bootstrap.min.js',
+  'admin/js/raphael-min.js',
+  'admin/js/plugins/morris/morris.min.js',
+  'admin/js/plugins/sparkline/jquery.sparkline.min.js',
+  'admin/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+  'admin/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+  'admin/js/plugins/fullcalendar/fullcalendar.min.js',
+  'admin/js/plugins/jqueryKnob/jquery.knob.js',
+  'admin/js/plugins/daterangepicker/daterangepicker.js',
+  'admin/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+  'admin/js/plugins/iCheck/icheck.min.js',
+  'admin/js/AdminLTE/app.js',
+  'admin/js/AdminLTE/dashboard.js'
 ];
 
 // Client-side HTML templates are injected using the sources below
