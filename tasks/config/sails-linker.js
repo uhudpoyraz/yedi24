e86716e407c89959885df4aa586164575372291c
uhudpoyraz <uhud.poyraz@gmail.com>
2016-04-23 04:03:48 +0300

    BinaController crud işlemleri tamamlandı.

--- tasks/config/sails-linker.js
+++ tasks/config/sails-linker.js
@@ -1,30 +1,14 @@
 /**
- * `sails-linker`
+ * Autoinsert script tags (or other filebased tags) in an html file.
  *
  * ---------------------------------------------------------------
  *
- * Automatically inject <script> tags and <link> tags into the specified
- * specified HTML and/or EJS files.  The specified delimiters (`startTag`
- * and `endTag`) determine the insertion points.
- *
- * #### Development (default)
- * By default, tags will be injected for your app's client-side JavaScript files,
- * CSS stylesheets, and precompiled client-side HTML templates in the `templates/`
- * directory (see the `jst` task for more info on that).  In addition, if a LESS
- * stylesheet exists at `assets/styles/importer.less`, it will be compiled to CSS
- * and a `<link>` tag will be inserted for it.  Similarly, if any Coffeescript
- * files exists in `assets/js/`, they will be compiled into JavaScript and injected
- * as well.
- *
- * #### Production (`NODE_ENV=production`)
- * In production, all stylesheets are minified into a single `.css` file (see
- * `tasks/config/cssmin.js` task) and all client-side scripts are minified into
- * a single `.js` file (see `tasks/config/uglify.js` task).  Any client-side HTML
- * templates, CoffeeScript, or LESS files are bundled into these same two minified
- * files as well.
+ * Automatically inject <script> tags for javascript files and <link> tags
+ * for css files.  Also automatically links an output file containing precompiled
+ * templates using a <script> tag.
  *
  * For usage docs see:
- *   https://github.com/Zolmeister/grunt-sails-linker
+ *    https://github.com/Zolmeister/grunt-sails-linker
  *
  */
 module.exports = function(grunt) {
@@ -41,6 +25,20 @@ module.exports = function(grunt) {
         '.tmp/public/**/*.html': require('../pipeline').jsFilesToInject,
         'views/**/*.html': require('../pipeline').jsFilesToInject,
         'views/**/*.ejs': require('../pipeline').jsFilesToInject
+      }
+    },
+
+    devJsAdmin: {
+      options: {
+        startTag: '<!--SCRIPTS_ADMIN-->',
+        endTag: '<!--SCRIPTS_ADMIN END-->',
+        fileTmpl: '<script src="%s"></script>',
+        appRoot: '.tmp/public'
+      },
+      files: {
+        '.tmp/public/**/*.html': require('../pipeline').jsAdminFilesToInject,
+        'views/**/*.html': require('../pipeline').jsAdminFilesToInject,
+        'views/**/*.ejs': require('../pipeline').jsAdminFilesToInject
       }
     },
 
@@ -70,6 +68,20 @@ module.exports = function(grunt) {
         '.tmp/public/**/*.html': ['.tmp/public/min/production.min.js'],
         'views/**/*.html': ['.tmp/public/min/production.min.js'],
         'views/**/*.ejs': ['.tmp/public/min/production.min.js']
+      }
+    },
+
+    prodJsAdmin: {
+      options: {
+        startTag: '<!--SCRIPTS_ADMIN-->',
+        endTag: '<!--SCRIPTS_ADMIN END-->',
+        fileTmpl: '<script src="%s"></script>',
+        appRoot: '.tmp/public'
+      },
+      files: {
+        '.tmp/public/**/*.html': ['.tmp/public/min/productionAdmin.min.js'],
+        'views/**/*.html': ['.tmp/public/min/productionAdmin.min.js'],
+        'views/**/*.ejs': ['.tmp/public/min/productionAdmin.min.js']
       }
     },
 
@@ -103,6 +115,21 @@ module.exports = function(grunt) {
       }
     },
 
+    devAdminStyles: {
+      options: {
+        startTag: '<!--ADMIN_STYLES-->',
+        endTag: '<!--ADMIN_STYLES END-->',
+        fileTmpl: '<link rel="stylesheet" href="%s">',
+        appRoot: '.tmp/public'
+      },
+
+      files: {
+        '.tmp/public/**/*.html': require('../pipeline').cssAdminFilesToInject,
+        'views/**/*.html': require('../pipeline').cssAdminFilesToInject,
+        'views/**/*.ejs': require('../pipeline').cssAdminFilesToInject
+      }
+    },
+
     devStylesRelative: {
       options: {
         startTag: '<!--STYLES-->',
@@ -130,6 +157,20 @@ module.exports = function(grunt) {
         '.tmp/public/index.html': ['.tmp/public/min/production.min.css'],
         'views/**/*.html': ['.tmp/public/min/production.min.css'],
         'views/**/*.ejs': ['.tmp/public/min/production.min.css']
+      }
+    },
+
+    prodAdminStyles: {
+      options: {
+        startTag: '<!--ADMIN_STYLES-->',
+        endTag: '<!--ADMIN_STYLES END-->',
+        fileTmpl: '<link rel="stylesheet" href="%s">',
+        appRoot: '.tmp/public'
+      },
+      files: {
+        '.tmp/public/index.html': ['.tmp/public/min/productionAdmin.min.css'],
+        'views/**/*.html': ['.tmp/public/min/productionAdmin.min.css'],
+        'views/**/*.ejs': ['.tmp/public/min/productionAdmin.min.css']
       }
     },
 
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -6,10 +6,8 @@
  *
  * (Note that you can take advantage of Grunt-style wildcard/glob/splat expressions
  * for matching multiple files.)
- *
- * For more information see:
- *   https://github.com/balderdashy/sails-docs/blob/master/anatomy/myApp/tasks/pipeline.js.md
  */
+
 
 
 // CSS files to inject in order
@@ -20,11 +18,14 @@ var cssFilesToInject = [
   'styles/**/*.css'
 ];
 
+var cssAdminFilesToInject = [
+  'admin/**/*.css'
+];
 
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-
+  
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
@@ -36,6 +37,32 @@ var jsFilesToInject = [
   'js/**/*.js'
 ];
 
+
+// Client-side javascript files to inject in order
+// (uses Grunt-style wildcard/glob/splat expressions)
+var jsAdminFilesToInject = [
+
+  // Load sails.io before everything else
+  //'jsAdmin/dependencies/sails.io.js',
+
+  // Dependencies like jQuery, or Angular are brought in here
+  
+  'admin/js/jquery.min.js',
+  'admin/js/jquery-ui-1.10.3.min.js',
+  'admin/js/bootstrap.min.js',
+  'admin/js/raphael-min.js',
+  'admin/js/plugins/morris/morris.min.js',
+  'admin/js/plugins/sparkline/jquery.sparkline.min.js',
+  'admin/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+  'admin/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+  'admin/js/plugins/fullcalendar/fullcalendar.min.js',
+  'admin/js/plugins/jqueryKnob/jquery.knob.js',
+  'admin/js/plugins/daterangepicker/daterangepicker.js',
+  'admin/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+  'admin/js/plugins/iCheck/icheck.min.js',
+  'admin/js/AdminLTE/app.js',
+  'admin/js/AdminLTE/dashboard.js'
+];
 
 // Client-side HTML templates are injected using the sources below
 // The ordering of these templates shouldn't matter.
@@ -52,24 +79,21 @@ var templateFilesToInject = [
 
 
 
-
-
-
-
-// Default path for public folder (see documentation for more information)
-var tmpPath = '.tmp/public/';
-
 // Prefix relative paths to source files so they point to the proper locations
 // (i.e. where the other Grunt tasks spit them out, or in some cases, where
 // they reside in the first place)
-module.exports.cssFilesToInject = cssFilesToInject.map(function(cssPath) {
-  return require('path').join('.tmp/public/', cssPath);
+module.exports.cssFilesToInject = cssFilesToInject.map(function(path) {
+  return '.tmp/public/' + path;
 });
-module.exports.jsFilesToInject = jsFilesToInject.map(function(jsPath) {
-  return require('path').join('.tmp/public/', jsPath);
+module.exports.cssAdminFilesToInject = cssAdminFilesToInject.map(function(path) {
+  return '.tmp/public/' + path;
 });
-module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPath) {
-  return require('path').join('assets/',tplPath);
+module.exports.jsFilesToInject = jsFilesToInject.map(function(path) {
+  return '.tmp/public/' + path;
 });
-
-
+module.exports.jsAdminFilesToInject = jsAdminFilesToInject.map(function(path) {
+  return '.tmp/public/' + path;
+});
+module.exports.templateFilesToInject = templateFilesToInject.map(function(path) {
+  return 'assets/' + path;
+});
